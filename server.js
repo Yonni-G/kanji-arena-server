743858f3c648c330b2e0b777c0a5337d9e2a4c29
Yonni GUERRAULT <yonni4@gmail.com>
2025-07-15 22:37:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const corsOptions = {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
+            console.log("Origin non autorisé :", origin);
             callback(new Error("Not allowed by CORS"));
         }
     },
